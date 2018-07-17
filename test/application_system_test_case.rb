b70d7dd151d4a4fff26134377b4536cbeddeeb39
Chap Ambrose <chapambrose@gmail.com>
2018-07-17 14:45:57 -0500

    this should break with chrome_shim_test error

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,19 +1,5 @@
 require "test_helper"
 
-# tell Heroku CI run where to find Chrome binary
-chrome_bin = ENV.fetch('GOOGLE_CHROME_BIN', nil)
-chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
-
-Capybara.register_driver :chrome_shim do |app|
-  Capybara::Selenium::Driver.new(
-     app,
-     browser: :chrome,
-     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
-  )
-end
-
-Capybara.javascript_driver = :chrome_shim
-
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :chrome_shim
+  driven_by :chrome_shim_test
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,20 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+# tell Heroku CI run where to find Chrome binary
+chrome_bin = ENV.fetch('GOOGLE_CHROME_BIN', nil)
+chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
+Capybara.register_driver :chrome_shim do |app|
+  Capybara::Selenium::Driver.new(
+     app,
+     browser: :chrome,
+     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+  )
+end
+
+Capybara.javascript_driver = :chrome_shim
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
