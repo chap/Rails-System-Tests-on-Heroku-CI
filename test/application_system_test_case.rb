1a6690afa9a4d92542af525474591198f9c315bf
Chap Ambrose <chapambrose@gmail.com>
2018-07-17 13:21:04 -0500

    specify chrome_shim for system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,16 @@
 require "test_helper"
 
+hrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+Capybara.register_driver :chrome_shim do |app|
+  Capybara::Selenium::Driver.new(
+     app,
+     browser: :chrome,
+     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+  )
+end
+Capybara.javascript_driver = :chrome_shim
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :chrome_shim
 end
