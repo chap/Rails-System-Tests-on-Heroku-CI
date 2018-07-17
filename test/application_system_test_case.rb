62969b8ab986cdebafb4f7df7e8e1a0851c1487a
Chap Ambrose <chapambrose@gmail.com>
2018-07-17 13:38:25 -0500

    test again

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,10 +1,10 @@
 require "test_helper"
 
 # tell Heroku CI run where to find Chrome binary
-chrome_bin  = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
 chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
 
-Capybara.register_driver :chrome do |app|
+Capybara.register_driver :chrome_shim do |app|
   Capybara::Selenium::Driver.new(
      app,
      browser: :chrome,
@@ -12,8 +12,8 @@ Capybara.register_driver :chrome do |app|
   )
 end
 
-Capybara.javascript_driver = :chrome
+Capybara.javascript_driver = :chrome_shim
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :chrome
+  driven_by :chrome_shim
 end
