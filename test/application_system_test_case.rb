0e54d17a3af2fb40e723f6339931485cca9938b9
Chap Ambrose <chapambrose@gmail.com>
2018-07-17 15:47:12 -0500

    Update application_system_test_case.rb

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,9 +1,10 @@
 require "test_helper"
 
-
 # tell Heroku CI run where to find chrome binary
+# https://github.com/heroku/heroku-buildpack-google-chrome#selenium
 chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
 chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
 Capybara.register_driver :chrome_shim do |app|
   Capybara::Selenium::Driver.new(
      app,
@@ -11,6 +12,7 @@ Capybara.register_driver :chrome_shim do |app|
      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
   )
 end
+
 Capybara.javascript_driver = :chrome_shim
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
