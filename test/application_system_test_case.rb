8548cce4bd5dbef1593039f521389fe35a5df334
Chap Ambrose <chapambrose@gmail.com>
2018-07-17 15:10:32 -0500

    so confused

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,18 @@
 require "test_helper"
 
+
+# tell Heroku CI run where to find chrome binary
+chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+Capybara.register_driver :chrome_shim do |app|
+  Capybara::Selenium::Driver.new(
+     app,
+     browser: :chrome,
+     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+  )
+end
+Capybara.javascript_driver = :chrome_shim
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :chrome
+  driven_by :chrome_shim
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,20 +2,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
-
-chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
-
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(
-     app,
-     browser: :chrome,
-     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
-  )
-end
-
-Capybara.javascript_driver = :chrome
-
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
