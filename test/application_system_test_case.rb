7cfa4160b938784532d8b1f595ceb2252402c339
Chap Ambrose <chapambrose@gmail.com>
2018-07-17 15:36:24 -0500

    try upgrading and using goog:chromeOptions

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,7 +3,7 @@ require "test_helper"
 
 # tell Heroku CI run where to find chrome binary
 chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
-chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+chrome_opts = chrome_bin ? { "goog:chromeOptions" => { "binary" => chrome_bin } } : {}
 Capybara.register_driver :chrome_shim do |app|
   Capybara::Selenium::Driver.new(
      app,
